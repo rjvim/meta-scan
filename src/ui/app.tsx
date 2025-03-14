6278f2cc76c7c7f0058ae39bd9138be7a5490585
Rajiv Seelam <rajiv@betalectic.com>
2025-03-14 18:04:54 +0530

    feat: update MenuIcon SVG dimensions and paths for improved design

--- src/ui/app.tsx
+++ src/ui/app.tsx
@@ -50,18 +50,19 @@ const MoonIcon = () => (
 
 const MenuIcon = () => (
   <svg
-    width="16"
-    height="16"
+    xmlns="http://www.w3.org/2000/svg"
+    width={24}
+    height={24}
     viewBox="0 0 24 24"
     fill="none"
     stroke="currentColor"
-    strokeWidth="2"
+    strokeWidth={2}
     strokeLinecap="round"
     strokeLinejoin="round"
+    className="lucide lucide-square-mouse-pointer"
   >
-    <path d="M4 12h16" />
-    <path d="M4 6h16" />
-    <path d="M4 18h16" />
+    <path d="M12.034 12.681a.498.498 0 0 1 .647-.647l9 3.5a.5.5 0 0 1-.033.943l-3.444 1.068a1 1 0 0 0-.66.66l-1.067 3.443a.5.5 0 0 1-.943.033z" />
+    <path d="M21 11V5a2 2 0 0 0-2-2H5a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h6" />
   </svg>
 );
 
